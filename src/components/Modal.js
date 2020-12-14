c9aa85a627c4faf2820d3b0ca6aeeaa5372468aa
M Umar Khan <umar.khan@arbisoft.com>
2020-12-15 00:44:45 +0500

    Successfull map function

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
 const Modal = ({ handleClose, show, values }) => {
-  const showHideClassName = show ? "my-modal display-block" : "my-modal display-none";
+  if (values === [] || values.course === undefined) {
+    return <div></div>
+  }
 
+  var handleChange = (e) => {
+    values.catalog_name = e.target.value;
+  }
+
+  const showHideClassName = show ? "my-modal display-block" : "my-modal display-none";
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
-        <p>Catalog Name: <span>{values.catalog_name}</span></p>
+        <p>Catalog Name: <input type="text" value={values.catalog_name} onChange={handleChange}/></p>
         <p>Catalog Type: <span>{values.type}</span></p>
         <p>Site: <span>{values.site}</span></p>
         <p>Course:</p>
-        <select>
-
+        <select multiple>
+        {values.course.map(i => <option value="{i}">{i}</option>)}
         </select>
         <button type="button" className="btn btn-primary" onClick={handleClose}>close</button>
       </section>
