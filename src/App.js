2a3aa9d1069df4d27ec95b8ffa19debd536c99f5
M Umar Khan <umar.khan@arbisoft.com>
2020-12-14 12:39:00 +0500

    Add search based table

--- src/App.js
+++ src/App.js
@@ -12,25 +12,15 @@ function App() {
   return (
     <div className="App">
 
-      <h3>Basic Table using <code>react-table</code></h3>
 
-      <BasicTableComponent />
+
+
 
       <h3>Filter Table using <code>react-table</code></h3>
 
       <FilterTableComponent />
 
-      <h3>Table with Pagination using <code>react-table</code></h3>
 
-      <PaginationTableComponent />
-
-      <h3>Sorting on Table using <code>react-table</code></h3>
-
-      <SortingTableComponent />
-
-      <h3>Expandable on Table using <code>react-table</code></h3>
-
-      <ExpandableTableComponent />
 
     </div>
   );
