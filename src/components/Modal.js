1ab92cb0b7fe3a9df151b05fd94735617b07565d
M Umar Khan <umar.khan@arbisoft.com>
2020-12-15 01:23:44 +0500

    Unable to change input catalog type

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Modal = ({ handleClose, show, values }) => {
+  const [catalogName, setCatalogName] = useState('hahaha');
   if (values === [] || values.course === undefined) {
     return <div></div>
   }
-
-  var handleChange = (e) => {
-    values.catalog_name = e.target.value;
+  if (catalogName !== values.catalog_name) {
+    setCatalogName(values.catalog_name);
   }
-
+  var handleChange = (e) => {
+    setCatalogName(e.target.value)
+  }
   const showHideClassName = show ? "my-modal display-block" : "my-modal display-none";
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
-        <p>Catalog Name: <input type="text" value={values.catalog_name} onChange={handleChange}/></p>
+        <p>Catalog Name: <input type="text" value={catalogName} onChange={handleChange}/></p>
         <p>Catalog Type: <span>{values.type}</span></p>
         <p>Site: <span>{values.site}</span></p>
         <p>Course:</p>
